983f208ef87df9f1d77dcf4ba6453689960643b8
Vishalkumar Chaudhary <vishalrcop@gmail.com>
2018-01-02 17:11:57 +0530

    CV in index

---
+++ index.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="css/circle.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+        <style>
+            .p{
+                padding-left: 23px   
+            }
+            .name{
+                text-align: center
+            }
+        </style>
+    </head>
+    <body style="margin-left:200px;margin-right:200px;margin-top:25px;margin-bottom:25px;border: 1px solid black">
+        <header>
+            <div style="height:189px;">
+                <div class="jumbotron" style="padding-top:30px;padding-bottom:30px">
+                  <h1>Karan Sampatlal Deopura</h1>
+                  <span>(+91) 9409156640</span><br>
+                  <span>karan.deopura@hotmail.com</span><br>
+                  <span>13 Geetanjali Society, New VIP Road, Vadodara-390022, Gujarat, India</span>
+                </div>
+            </div>
+        </header>
+        <div class="content-wrapper">
+            <div class="row" style="padding-left:15px;padding-right:15px;">
+                <div class="col-md-4" style="background-color:beige">
+                    <section style="padding-left:35px;padding-right:20px;padding-top:20px;padding-bottom:20px">
+                        <div class="row">
+                            <img src="F6EQz67.jpg" class="img-thumbnail" width="290" alt="Cinque Terre">
+                        </div>
+                    </section>
+                    <section style="padding-left:35px;padding-right:20px;padding-top:20px;padding-bottom:20px">
+                        <div class="row"  style="border-bottom: 1px solid gray">
+                            <h4>Personal</h4>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <div class="col-md-4" style="padding-left:0px">
+                                <span>Birthday</span>
+                            </div>
+                            <div class="col-md-8">
+                                <span>6<sup>th</sup> October 1996</span>
+                            </div>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <div class="col-md-4" style="padding-left:0px">
+                                <span>Citizenship</span>
+                            </div>
+                            <div class="col-md-8">
+                                <span>Indian</span>
+                            </div>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <div class="col-md-4" style="padding-left:0px">
+                                <span>Gender</span>
+                            </div>
+                            <div class="col-md-8">
+                                <span>Male</span>
+                            </div>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <div class="col-md-4" style="padding-left:0px">
+                                <span>Relationship</span>
+                            </div>
+                            <div class="col-md-8">
+                                <span>Single</span>
+                            </div>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <div class="col-md-4" style="padding-left:0px">
+                                <span>Languages</span>
+                            </div>
+                            <div class="col-md-8">
+                                <span>Hindi, Gujarati, English</span>
+                            </div>
+                        </div>
+                    </section>
+                    
+                    <section style="padding-left:35px;padding-right:20px;padding-top:20px;padding-bottom:20px">
+                        <div class="row"  style="border-bottom: 1px solid gray">
+                            <h4>Area of Interest</h4>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Computer Science</span>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Machine Learning</span>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Android Development</span>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Website development</span>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Programming Language</span>
+                        </div>
+                    </section>
+                    
+                    <section style="padding-left:35px;padding-right:20px;padding-top:20px;padding-bottom:20px">
+                        <div class="row"  style="border-bottom: 1px solid gray">
+                            <h4>References</h4>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Computer Science</span>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Machine Learning</span>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Android Development</span>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Website development</span>
+                        </div>
+                        <div class="row" style="padding-top:5px;border-bottom:1px solid gray">
+                            <span>Programming Language</span>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-8">
+                    <section style="padding-left:20px;padding-right:15px;padding-top:20px;padding-bottom:20px">
+                        <div style="padding-left:15px;padding-right:15px">
+                            <div class="row" style="border-bottom: 1px solid gray"><h4>Profile</h4></div>
+                        </div>
+                        <span>
+                            To learn something is my first objective.Also, to work in professional work driven
+                            challenging and dynamic environment where I can utilize and apply my knowledge,
+                            skills with maximum potential would enable me as a fresh graduate to grow while
+                            fulfilling organizational goals and pursue in the field of computer science
+                        </span>
+                    </section>
+                    <section style="padding-left:20px;padding-right:15px;padding-top:20px;padding-bottom:20px">
+                        <div class="row">
+                            <div class="clearfix col-md-3">
+                                <div class="c100 p25">
+                                <span>25%</span>
+                                <div class="slice">
+                                    <div class="bar"></div>
+                                    <div class="fill"></div>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="clearfix col-md-3">
+                                <div class="c100 p25">
+                                <span>25%</span>
+                                <div class="slice">
+                                    <div class="bar"></div>
+                                    <div class="fill"></div>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="clearfix col-md-3">
+                                <div class="c100 p57">
+                                <span>25%</span>
+                                <div class="slice">
+                                    <div class="bar"></div>
+                                    <div class="fill"></div>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="clearfix col-md-3">
+                                <div class="c100 p25">
+                                <span>25%</span>
+                                <div class="slice">
+                                    <div class="bar"></div>
+                                    <div class="fill"></div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" >
+                            <div class="col-md-3"><div class="p">Teamwork</div></div>
+                            <div class="col-md-3"><div class="p">Teamwork</div></div>
+                            <div class="col-md-3"><div class="p">Teamwork</div></div>
+                            <div class="col-md-3"><div class="p">Teamwork</div></div>
+                        </div>
+                    </section>
+                    <section style="padding-left:20px;padding-right:15px;padding-top:20px;padding-bottom:20px">
+                        <div style="padding-left:15px;padding-right:15px">
+                            <div class="row" style="border-bottom: 1px solid gray"><h4>Hobbies</h4></div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <footer>
+        
+        </footer>
+            <!-- /default -->
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+      <!-- Popper JS -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+
+      <!-- Latest compiled JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    </body>
+</html>
