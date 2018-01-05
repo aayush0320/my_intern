a0b730b54fbbbc583c5adfbca4e645b69fa96295
Vishalkumar Chaudhary <vishalrcop@gmail.com>
2018-01-05 23:11:27 +0530

    layout changes in patient.php
    Front end changes

--- php/patient.php
+++ php/patient.php
@@ -19,7 +19,7 @@
             <div class="box-header with-border">    
                 <!-- Content Header (Page header) -->
                 <section class="content-header">
-                  <h1>
+                  <h1 style="text-align:center">
                       OPD FORM
                   </h1>
                 </section>
@@ -27,12 +27,12 @@
             <!-- /.box-header -->
             <div class="box-body">
               <form role="form">
+                <h3 style=" padding-bottom:20px">Personal Information</h3>
                 <!-- text input -->
                 <div class="form-group">
                     <div class="row">
-                        <div class="col-md-1"></div>
                         <div class="col-md-2">
-                            <label>Name</label>
+                            <div style="float:right"><label>Name</label></div>
                         </div>
                         <div class="col-md-8">
                             <input type="text" id="" class="form-control" placeholder="Enter ...">
@@ -40,24 +40,54 @@
                     </div> 
                 </div>
                 <div class="form-group">
-                  <label>Age</label>
-                  <input type="text" id="" class="form-control" placeholder="Enter ...">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div style="float:right"><label>Age</label></div>
+                        </div>
+                        <div class="col-md-8">
+                            <input type="text" id="" class="form-control" placeholder="Enter ...">
+                        </div>
+                    </div> 
                 </div>
                 <div class="form-group">
-                  <label>Sex</label>
-                  <input type="text" id="" class="form-control" placeholder="Enter ...">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div style="float:right"><label>Sex</label></div>
+                        </div>
+                        <div class="col-md-8">
+                            <input type="text" id="" class="form-control" placeholder="Enter ...">
+                        </div>
+                    </div> 
                 </div>
                 <div class="form-group">
-                  <label>Contact</label>
-                  <input type="text" id="" class="form-control" placeholder="Enter ...">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div style="float:right"><label>Contact</label></div>
+                        </div>
+                        <div class="col-md-8">
+                            <input type="text" id="" class="form-control" placeholder="Enter ...">
+                        </div>
+                    </div> 
                 </div>
                 <div class="form-group">
-                  <label>Prime</label>
-                  <input type="text" id="" class="form-control" placeholder="Enter ...">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div style="float:right"><label>Prime</label></div>
+                        </div>
+                        <div class="col-md-8">
+                            <input type="text" id="" class="form-control" placeholder="Enter ...">
+                        </div>
+                    </div> 
                 </div>
                 <div class="form-group">
-                  <label>Address</label>
-                  <input type="text" id="" class="form-control" placeholder="Enter ...">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div style="float:right"><label>Address</label></div>
+                        </div>
+                        <div class="col-md-8">
+                            <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                        </div>
+                    </div> 
                 </div>                  
 <!--
                 <div class="form-group">
@@ -67,25 +97,76 @@
 -->
                 <!-- textarea -->
                 <hr>
+                <h3 style=" padding-bottom:20px">Diagnosis</h3>
                 <div class="form-group">
-                  <label>Diagnosis</label>
-                  <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div style="float:right"><label>Eleboration</label></div>
+                        </div>
+                        <div class="col-md-8">
+                            <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                        </div>
+                    </div> 
                 </div>
                 <hr>
+                <h3 style=" padding-bottom:20px">Reports</h3>
                 <div class="form-group">
-                  <label>Reports</label>
-                  <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div style="float:right"><label>List</label></div>
+                        </div>
+                        <div class="col-md-8">
+                            <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                        </div>
+                    </div> 
                 </div>
                 <hr>
-                <div>
-                  <label>Treatment</label>
-                    <div class="input-group input-group-sm">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                          <button type="button" id="tab" class="btn btn-info btn-flat">Go!</button>
-                        </span>
-                    </div>
+                <h3 style=" padding-bottom:20px">Treatment</h3>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div style="float:right"><label>Medicine Name</label></div>
+                        </div>
+                        <div class="col-md-4">
+                            <input type="text" id="medicine_name" class="form-control" placeholder="Enter ...">
+                        </div>
+                        <div class="col-md-3">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input id="morning" type="checkbox">
+                                                Morning
+                                            </label>
+                                        </div>               
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input id="noon" type="checkbox">
+                                                Noon
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input id="night" type="checkbox">
+                                                Night
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <input type="text" id="course" class="form-control" placeholder="Enter ...">
+                                    </div>
+                                </div>
+                        </div>
+                        <div class="col-md-1">
+                            <button type="button" id="medicine_button" class="btn btn-block btn-info">Add</button>
+                        </div>
+                    </div> 
                 </div>
+                  
               </form>
             </div>
         </div>
@@ -96,5 +177,19 @@
 </div>
 <!-- ./wrapper -->
 <?php include '../html/js.html';?>
+<script>
+    var x = document.getElementsById("tab-in");
+    ("#tab-but").click(function(){
+       $ajax({
+          url:"../ajax/tablet-dropdowns.php",
+          data:'data ='+x,
+          type:"POST",
+          success:function(result)
+           {
+                $(".drop-down").html(result);
+           }
+       }); 
+    });
+</script>
 </body>
 </html>
